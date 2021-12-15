a8cbb8768854776bb5cf6d73691ac205213f89a2
alungseptiann <s160418055@student.ubaya.ac.id>
2021-12-15 21:15:30 +0700

    done2

--- app/src/main/java/id/ac/ubaya/informatika/a160418055_septian_foodrecipes/view/AddRecipesFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/a160418055_septian_foodrecipes/view/AddRecipesFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import id.ac.ubaya.informatika.a160418055_septian_foodrecipes.R
+import kotlinx.android.synthetic.main.fragment_add_recipes.*
+
 class AddRecipesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -13,5 +16,9 @@ class AddRecipesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_recipes, container, false)
+
+        btnAddRecipes.setOnClickListener {
+            Navigation.findNavController(it).popBackStack()
+        }
     }
 }
